376a392c8d2147db43def39f4c58ccdfeaee3a9a
Daffa Akhadi Yoga Perdana <akhadidaffa13@gmail.com>
2025-10-22 14:29:52 +0700

    perbaiki navbar

--- resources/views/partials/navbar-after-login.blade.php
+++ resources/views/partials/navbar-after-login.blade.php
@@ -62,12 +62,11 @@
                     </div>
                 </div>
                 
-
                 <div class="dropdown">
                     <button class="btn dropdown-toggle d-flex align-items-center" type="button" id="userDropdown" 
                             data-bs-toggle="dropdown" aria-expanded="false" style="background: none; border: none; color: white;"
                             onclick="toggleUserDropdown()">
-                         <img src="{{ optional(Auth::user())->avatar_url ?? asset('images/default-avatar.png') }}"
+                        <img src="{{ Auth::user()->avatar_url }}"
                             alt="Avatar" class="rounded-circle me-2"
                             style="width:40px; height:40px; object-fit:cover; border:2px solid #fff; background:#eee;">
                         <span class="text-white">{{ Auth::user()->name }}</span>
